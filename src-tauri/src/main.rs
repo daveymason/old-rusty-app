3a383ff24a062b60b85a068704ff8e7b8afdd809
Davey Mason <daveymason@outlook.com>
2024-07-20 01:13:35 +0200

    Encryption made simple

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,10 +4,12 @@ use std::process::Command;
 use tauri::command;
 use reqwest::blocking::Client;
 use serde_json::Value;
+use base64::{encode, decode};
+use std::str;
 
 #[command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've connected HTMX to RUST. Nice huh?", name)
+    format!("Hello, {}! Using HTMX & Rust is nice eh?", name)
 }
 
 #[command]
@@ -33,14 +35,11 @@ fn scan_network(ip: &str) -> Result<String, String> {
 #[command]
 fn search_patients() -> Result<String, String> {
     let client = Client::new();
-    let response = client.get("https://r4.smarthealthit.org/Patient?_count=5")
-        .send();
-
-    match response {
-        Ok(resp) => {
-            let json: Value = resp.json().unwrap();
-            Ok(json.to_string())
-        }
+    match client.get("https://r4.smarthealthit.org/Patient?_count=5").send() {
+        Ok(resp) => match resp.json::<Value>() {
+            Ok(json) => Ok(json.to_string()),
+            Err(_) => Err("Failed to parse JSON response from server".into()),
+        },
         Err(_) => Err("Failed to search patients".into()),
     }
 }
@@ -49,21 +48,34 @@ fn search_patients() -> Result<String, String> {
 fn fetch_patient_details(patient_id: &str) -> Result<String, String> {
     let client = Client::new();
     let url = format!("https://r4.smarthealthit.org/Patient/{}", patient_id);
-    let response = client.get(&url)
-        .send();
-
-    match response {
-        Ok(resp) => {
-            let json: Value = resp.json().unwrap();
-            Ok(json.to_string())
-        }
+    match client.get(&url).send() {
+        Ok(resp) => match resp.json::<Value>() {
+            Ok(json) => Ok(json.to_string()),
+            Err(_) => Err("Failed to parse JSON response from server".into()),
+        },
         Err(_) => Err("Failed to fetch patient details".into()),
+    }
+}
+
+#[command]
+fn encrypt(message: &str) -> Result<String, String> {
+    Ok(encode(message))
+}
+
+#[command]
+fn decrypt(message: &str) -> Result<String, String> {
+    match decode(message) {
+        Ok(bytes) => match str::from_utf8(&bytes) {
+            Ok(decoded_str) => Ok(decoded_str.to_string()),
+            Err(_) => Err("Failed to convert decoded bytes to string".into()),
+        },
+        Err(_) => Err("Failed to decode base64 message".into()),
     }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, reset, scan_network, search_patients, fetch_patient_details])
+        .invoke_handler(tauri::generate_handler![greet, reset, scan_network, search_patients, fetch_patient_details, encrypt, decrypt])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
